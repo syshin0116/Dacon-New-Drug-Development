9aabbcbe6093ec36474e5723e3957678535a9da5
syshin0116 <syshin0116@gmail.com>
2022-03-03 17:55:53 +0900

    python list

--- pythonProject02/list/list04.py
+++ pythonProject02/list/list04.py
@@ -1,3 +1,5 @@
+# Battleship game 신승엽
+
 import random
 
 bship1 = []
@@ -5,20 +7,22 @@ bship2 = []
 bship1_shot = []
 hitcount1 = 0
 hitcount2 = 0
+#플레이어, 컴퓨터, 쏜 자리 빈칸(0)으로 만듬
 for i in range(20):
     bship1.append(0)
     bship2.append(0)
     bship1_shot.append(0)
 
+# 플레이어진영, 쏜자리 그림
 print()
 print(' ', end='')
 for x in range(1,10):
     print(x, end='  ')
 for x in range(10,21):
     print(x, end=' ')
+
 print()
 print(bship1)
-
 print("플레이어 진영")
 print('---------------------------------------------------------------')
 print("플레이거가 쏜 자리")
@@ -32,19 +36,23 @@ print()
 print()
 
 
-
+#플레이어 배 위치 선정
 # p1_list = [input() for _ in range(3)]
 p1 = input("플레이어1) 선정할 배 위치 좌표 3개를 입력해주세요 ex)1,2,3 \n")
-p2 = random.randint(1,20)
+
 p1_list = p1.split(',')
 p1_list = [int(n) for n in p1_list]
-
 for i in p1_list:
     bship1[(i-1)] = 1
+#컴퓨터 배 위치 랜덤으로 선정
+
+p2 = random.randint(1,20)
 bship2[(p2-1)] = 1
 bship2[(p2)] = 1
 bship2[(p2+1)] = 1
 
+
+# 플레이어진영, 쏜자리 그림
 print(' ', end='')
 for x in range(1,10):
     print(x, end='  ')
@@ -64,6 +72,7 @@ for x in range(10,21):
 print()
 print()
 
+##게임 시작
 print("선공입니다")
 while True:
     print(' ', end='')
