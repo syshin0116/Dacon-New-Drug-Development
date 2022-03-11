fca95f8b67d8707a230a15de025012c1e0596167
syshin0116 <syshin0116@gmail.com>
2022-03-11 20:14:45 +0900

    openAPI, turtle, car

--- pythonProject07/openAPI/multitag_kakao_vision2.py
+++ pythonProject07/openAPI/multitag_kakao_vision2.py
@@ -1,5 +1,7 @@
 import sys
 import argparse
+from tkinter import messagebox
+
 import requests #알트+엔터(cmd+1)
 from collections import Counter
 
@@ -25,6 +27,11 @@ if __name__ == '__main__':
     img_list = ['https://img.imbc.com/template/2021/11/program_ddef4556-bde9-42ee-b5d0-f9700326c252.jpg',
                 'https://img2.sbs.co.kr/img/sbs_cms/WE/2021/07/12/3DL1626054692449.jpg',
                 'https://w.namu.la/s/1c0e615586d75cb6814b6c39e86f19b0e21917c8a3d62af2264667a34aae228cfae185c2365ab46369a4e6c9b0744ccd204da6868e51cd84e9f7176432595ade1484e5464963a16d2a79d8b25622901a',
+                'https://images.chosun.com/resizer/taI1PaMAxWVDBw3j1n0ZUoj0eB8=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/DTPZSPJJJRAWXLDE66PUOIVAF4.jpg',
+                'https://posri.re.kr/images/old_files/board/Iron100-2.jpg',
+                'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202110/14/2cff8a58-552f-49c2-b566-5ba73d343c96.jpg',
+                'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg',
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Causeway_Bay_sit-in.JPG/250px-Causeway_Bay_sit-in.JPG'
                 ]
     result_list = []
     for img in img_list:
@@ -41,3 +48,12 @@ if __name__ == '__main__':
     print(order_5[0]) #('사람', 3)
     order_1 = order_5[0]
     print('제일 반도수가 높은 단어는', order_1[0], '빈도수는', order_1[1]) # 제일 반도수가 높은 단어는 사람 빈도수는 3
+
+    tour = ''
+    if order_1[0] == '사람':
+        tour = '제주도'
+    elif order_1[0] == '남성':
+        tour = '등산'
+    else:
+        tour = '놀이공원'
+    messagebox.showinfo('추천', '당신에게 ' + tour + '를 추천합니다.')
